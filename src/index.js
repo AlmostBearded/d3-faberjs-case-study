8505e07f0dab36fbb7fa94a097a1e7c806f67e38
Peter Oberrauner <thealmostbearded@gmail.com>
2020-08-05 17:41:12 +0200

    Implement bar chart

---
+++ src/index.js
@@ -0,0 +1,234 @@
+import * as d3 from 'd3';
+import { parseNodeHierarchy, computeLayout, applyLayout } from './layout';
+
+// Data representing the populations of large Austrian cities
+var data = [
+  { city: 'Vienna', population: 1691000 },
+  { city: 'Graz', population: 222000 },
+  { city: 'Linz', population: 205000 },
+  { city: 'Salzburg', population: 153000 },
+  { city: 'Innsbruck', population: 132000 },
+  { city: 'Klagenfurt', population: 91000 },
+  { city: 'Villach', population: 59000 },
+];
+
+// Configuration for the chart
+var config = {
+  numericAxis: {
+    visible: true,
+    title: 'Population',
+    titleMargin: 10,
+  },
+  categoricAxis: {
+    visible: true,
+    title: 'Countries',
+    titleMargin: 10,
+  },
+  margin: 20,
+};
+
+// Calculate bounding rect of the chart
+var boundingRect = getBoundingRect('#chart');
+
+// Setting up the initial scales for the full size of the container
+var categoricScale = d3
+  .scaleBand()
+  .domain(data.map((d) => d.city))
+  .padding(0.05)
+  .range([0, boundingRect.width]);
+
+var numericScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data, (d) => d.population)])
+  .range([boundingRect.height, 0]);
+
+// Create the root node of the chart
+var svgSelection = d3.select('#chart').append('svg');
+
+// Cached selection variables
+var numericAxisSelection, categoricAxisSelection, barsSelection;
+
+// Create an initial scaffold of the chart that reflects the desired layout
+scaffoldChart();
+
+// Initial rendering of the axes to access their sizes during layouting
+renderCategoricAxis();
+renderNumericAxis();
+
+// Parse the DOM hierarchy
+var layoutDOMNodes = [];
+var layoutNodes = [];
+parseNodeHierarchy(svgSelection.node(), layoutDOMNodes, layoutNodes);
+// console.log(layoutDOMNodes);
+// console.log(layoutNodes);
+
+// Update the layout to fit into the bounding rect dimensions
+updateLayout();
+
+window.addEventListener('resize', updateLayout);
+
+// Get the bounding rect of a node
+function getBoundingRect(selector) {
+  var node = document.querySelector(selector);
+  return node.getBoundingClientRect();
+}
+
+// Scaffold the desired layout of the chart
+function scaffoldChart() {
+  // This function has been organized using unnecessary blocks to reflect nesting of nodes
+
+  svgSelection.each(function () {
+    this.layoutStyle = {
+      display: 'grid',
+      gridTemplateColumns: `[.] ${config.margin} [axis] auto [bars] 1fr [.] ${config.margin}`,
+      gridTemplateRows: `[.] ${config.margin} [bars] 1fr [axis] auto [.] ${config.margin}`,
+      justifyItems: 'stretch',
+      alignItems: 'stretch',
+    };
+  });
+  {
+    numericAxisSelection = svgSelection
+      .append('g')
+      .classed('axis left-axis numeric-axis', true)
+      .each(function () {
+        this.layoutStyle = {
+          display: 'grid',
+          height: 'layout',
+          gridTemplateColumns: `[title] auto [margin] ${config.numericAxis.titleMargin} [ticks] auto`,
+          gridColumn: 'axis / span 1',
+          gridRow: 'bars / span 1',
+        };
+      });
+    {
+      numericAxisSelection
+        .append('text')
+        .classed('title', true)
+        .each(function () {
+          this.layoutStyle = {
+            width: 'auto',
+            height: 'auto',
+            gridColumn: 'title / span 1',
+            gridRow: '1 / span 1',
+            alignSelf: 'center',
+          };
+        });
+      numericAxisSelection
+        .append('g')
+        .classed('ticks', true)
+        .each(function () {
+          this.layoutStyle = { width: 'auto', gridColumn: 'ticks / span 1', gridRow: '1 / span 1' };
+        });
+    }
+    barsSelection = svgSelection
+      .append('g')
+      .classed('bars', true)
+      .each(function () {
+        this.layoutStyle = { gridColumn: 'bars / span 1', gridRow: 'bars / span 1' };
+      });
+  }
+  categoricAxisSelection = svgSelection
+    .append('g')
+    .classed('axis bottom-axis categoric-axis', true)
+    .each(function () {
+      this.layoutStyle = {
+        width: 'layout',
+        display: 'grid',
+        gridTemplateRows: `[ticks] auto [margin] ${config.categoricAxis.titleMargin} [title] auto`,
+        gridColumn: 'bars / span 1',
+        gridRow: 'axis / span 1',
+      };
+    });
+  {
+    categoricAxisSelection
+      .append('g')
+      .classed('ticks', true)
+      .each(function () {
+        this.layoutStyle = {
+          height: 'auto',
+          gridColumn: '1 / span 1',
+          gridRow: 'ticks / span 1',
+        };
+      });
+    categoricAxisSelection
+      .append('text')
+      .classed('title', true)
+      .each(function () {
+        this.layoutStyle = {
+          height: 'auto',
+          width: 'auto',
+          gridColumn: '1 / span 1',
+          gridRow: 'title / span 1',
+          justifySelf: 'center',
+        };
+      });
+  }
+}
+
+function renderCategoricAxis() {
+  categoricAxisSelection.attr('transform', `scale(${+config.categoricAxis.visible}, 1)`);
+
+  categoricAxisSelection
+    .select('.ticks')
+    .call(d3.axisBottom(categoricScale))
+    .attr('font-size', null)
+    .attr('font-family', null);
+
+  categoricAxisSelection
+    .select('.title')
+    .attr('dy', '0.7em')
+    .text(config.categoricAxis.title || '');
+}
+
+function renderNumericAxis() {
+  numericAxisSelection.attr('transform', `scale(${+config.numericAxis.visible}, 1)`);
+
+  numericAxisSelection
+    .select('.title')
+    .attr('text-anchor', 'end')
+    .attr('dy', '0.7em')
+    .attr('transform', 'rotate(-90)')
+    .text(config.numericAxis.title || '');
+
+  numericAxisSelection
+    .select('.ticks')
+    .call(d3.axisLeft(numericScale))
+    .attr('font-size', null)
+    .attr('font-family', null)
+    .call((e) => e.attr('transform', `translate(${e.node().getBoundingClientRect().width}, 0)`));
+}
+
+function renderBars() {
+  barsSelection
+    .attr('fill', 'steelblue')
+    .selectAll('rect')
+    .data(data)
+    .join('rect')
+    .attr('x', (d) => categoricScale(d.city))
+    .attr('y', (d) => numericScale(d.population))
+    .attr('height', (d) => numericScale(0) - numericScale(d.population))
+    .attr('width', categoricScale.bandwidth());
+}
+
+function updateLayout() {
+  // Update the size of the bounding rect
+  boundingRect = getBoundingRect('#chart');
+
+  // Update the viewbox of the chart
+  svgSelection.attr('viewBox', `0, 0, ${boundingRect.width}, ${boundingRect.height}`);
+
+  // Calculate the layout
+  computeLayout(boundingRect, layoutDOMNodes, layoutNodes);
+
+  // Resize the range of the scale to fit into the calculated size of the bar drawing area
+  var barsLayout = layoutNodes[layoutDOMNodes.indexOf(barsSelection.node().parentNode)].layout;
+  categoricScale.range([0, barsLayout.width]);
+  numericScale.range([barsLayout.height, 0]);
+
+  // Rerender the axes and render the bars now that the scales have correct ranges
+  renderCategoricAxis();
+  renderNumericAxis();
+  renderBars();
+
+  // Position the different nodes according to the layout
+  applyLayout(layoutDOMNodes, layoutNodes);
+}
---
+++ src/layout.js
@@ -0,0 +1,75 @@
+import { computeLayout as faberComputeLayout } from './faberjs';
+
+export function parseNodeHierarchy(node, nodes, layoutNodes) {
+  var style = JSON.parse(JSON.stringify(node.layoutStyle));
+
+  var layout = {
+    style,
+    children: [],
+  };
+
+  var groupNode = node;
+  // // The root node (SVG node) can't be wrapped in a group node
+  if (node.tagName !== 'svg') {
+    groupNode = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    groupNode.layoutStyle = node.layoutStyle;
+    node.parentNode.append(groupNode);
+    groupNode.append(node);
+  }
+
+  nodes.push(groupNode);
+  layoutNodes.push(layout);
+
+  // Must cache children because the DOM is modified recursively
+  var children = [...node.children];
+
+  for (var i = 0; i < children.length; ++i) {
+    var childNode = children[i];
+    if (!childNode.layoutStyle) break;
+    var childLayout = parseNodeHierarchy(childNode, nodes, layoutNodes);
+    layout.children.push(childLayout);
+  }
+
+  return layout;
+}
+
+export function computeLayout(size, nodes, layoutNodes) {
+  layoutNodes[0].style.width = size.width;
+  layoutNodes[0].style.height = size.height;
+
+  var dimensions = ['width', 'height'];
+
+  // Set 'auto' dimensions
+  for (var i = 0; i < nodes.length; ++i) {
+    for (var j = 0; j < dimensions.length; ++j) {
+      var value = nodes[i].layoutStyle[dimensions[j]];
+      if (!value) continue;
+      layoutNodes[i].style[dimensions[j]] =
+        value === 'auto' ? nodes[i].getBoundingClientRect()[dimensions[j]] : value;
+      // console.log(`${dimensions[j]}: ${value} → ${layoutNodes[i].style[dimensions[j]]}`);
+    }
+  }
+
+  faberComputeLayout(layoutNodes[0]);
+
+  // Set 'layout' dimensions
+  for (var i = 0; i < nodes.length; ++i) {
+    for (var j = 0; j < dimensions.length; ++j) {
+      var value = layoutNodes[i].style[dimensions[j]];
+      if (!value) continue;
+      layoutNodes[i].style[dimensions[j]] =
+        value === 'layout' ? layoutNodes[i].layout[dimensions[j]] : value;
+      // console.log(`${dimensions[j]}: ${value} → ${layoutNodes[i].style[dimensions[j]]}`);
+    }
+  }
+
+  faberComputeLayout(layoutNodes[0]);
+}
+
+export function applyLayout(nodes, layoutNodes) {
+  // Apply a translation on all laid out nodes to mirror the layout.
+  for (var i = 0; i < nodes.length; ++i) {
+    var layoutTransform = `translate(${layoutNodes[i].layout.x}, ${layoutNodes[i].layout.y})`;
+    nodes[i].setAttribute('transform', layoutTransform);
+  }
+}
